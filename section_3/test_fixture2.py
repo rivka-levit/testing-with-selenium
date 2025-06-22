3670c7511ca7e14f6e6bb166cab0efdb2d831e80
rivka-yuliia <juliacreadora@gmail.com>
2025-06-22 04:55:41 +0300

    create conftest.py

--- section_3/test_fixture2.py
+++ section_3/test_fixture2.py
@@ -1,17 +1,6 @@
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/"
-
-
-@pytest.fixture
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
 
 
 class TestMainPage1:
