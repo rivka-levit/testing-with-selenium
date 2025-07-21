8a1ec19a17327b479780564acb1ccb5e558a2c1c
rivka-yuliia <juliacreadora@gmail.com>
2025-07-21 17:34:25 +0300

    using double_click

--- training/test_action_chains.py
+++ training/test_action_chains.py
@@ -13,3 +13,10 @@ def test_drag_and_drop_by_offset(browser):
     actions = ActionChains(browser)
     actions.drag_and_drop_by_offset(peter, 0, -200).perform()
     print(browser.find_element(By.ID, 'password').text)
+
+
+def test_double_click(browser):
+    browser.get('https://parsinger.ru/selenium/7/7.3.2/index.html')
+    element = browser.find_element(By.ID, 'dblclick-area')
+    ActionChains(browser).double_click(element).perform()
+    print(browser.find_element(By.ID, 'password').text)
