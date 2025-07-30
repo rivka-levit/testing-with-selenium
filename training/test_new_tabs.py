1931c12c9492f69d74e4919d3a3fb0d645ee8c42
rivka-yuliia <juliacreadora@gmail.com>
2025-07-30 16:58:21 +0300

    switch to several tabs

--- training/test_new_tabs.py
+++ training/test_new_tabs.py
@@ -3,6 +3,11 @@ Test work with new tabs and windows.
 Command: pytest training\test_new_tabs.py
 """
 
+import time
+
+from selenium.webdriver.common.by import By
+
+
 def test_open_new_tabs(browser):
     browser.get('about:blank')
 
@@ -15,3 +20,27 @@ def test_open_new_tabs(browser):
     num2 = int(''.join([ch for ch in browser.title if ch not in {'7', '8', '0'}]))
 
     print(num1 + num2)
+
+
+def test_switch_to_tabs(browser):
+    browser.get('https://parsinger.ru/selenium/8/8.1.2/index.html')
+    elements_a = browser.find_elements(By.CSS_SELECTOR, '.code-links a')
+    links = [i.get_attribute('href') for i in elements_a]
+    result = 0
+
+    for link in links:
+        browser.switch_to.new_window('tab')
+        browser.get(link)
+
+    time.sleep(3)
+
+    for page in browser.window_handles[1:]:
+        browser.switch_to.window(page)
+        nums = browser.find_elements(By.CLASS_NAME, 'number')
+        result += sum([int(i.text) for i in nums])
+
+    browser.switch_to.window(browser.window_handles[0])
+    time.sleep(2)
+    browser.find_element(By.ID, 'sumInput').send_keys(str(result))
+    browser.find_element(By.ID, 'checkButton').click()
+    print(browser.find_element(By.ID, 'passwordDisplay').text)
